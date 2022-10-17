91abeb7d45fd92c9340d5f9457f3f733358547b2
GitHub Action <action@github.com>
2022-10-17 03:50:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/hdyztmdqd/juukplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/hdyztmdqd/juukplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hdyztmdqd.juukplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.hdyztmdqd.juukplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/hdyztmdqd/juukplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hdyztmdqd/juukplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hdyztmdqd.juukplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hdyztmdqd.juukplugin.MyBundle
 
 class MyApplicationService {
 
